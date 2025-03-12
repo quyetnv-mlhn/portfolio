0b46c2400a77b756bc6de430173e8d52727eaf78
quyetnv-mlhn <quyetnv-mlhn@users.noreply.github.com>
2025-03-12 01:09:54 +0000

    deploy: bf4bbe3a1a34251e61cc2b204639e85e367b5813

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -64,7 +64,7 @@ const RESOURCES = {"canvaskit/skwasm.js": "f2ad9363618c5f62e813740099a80e63",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "manifest.json": "d40c47d1c161f94dbcb13094d37f1f55",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
-"flutter_bootstrap.js": "877083546d6b2654191f343b21ab9a9f"};
+"flutter_bootstrap.js": "dd5d96719311b5bd7bbc688509987710"};
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = ["main.dart.js",
