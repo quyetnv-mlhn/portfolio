db7c9cfeae49188f353fd0205df92a651635aa3f
quyetnv <quyetnv.bhsoft@gmail.com>
2025-03-24 13:54:43 +0700

    feat: add introduction field to personal info and update related UI components

--- lib/gen/codegen_loader.g.dart
+++ lib/gen/codegen_loader.g.dart
@@ -16,47 +16,8 @@ class CodegenLoader extends AssetLoader {
 
   static const Map<String, dynamic> _en_US = {
     "appTitle": "Portfolio",
-    "home": {
-      "title": "Home",
-      "greeting": "Hi! I'm Quyet",
-      "role": "a 'Mobile Engineer'",
-      "sharing": {
-        "line1": "I will share",
-        "line2": "knowledge and",
-        "line3": "stories here",
-      },
-      "helping": {
-        "line1": "I hope to help",
-        "line2": "everyone with 'something'",
-        "line3": "while you are here",
-      },
-      "contact": {
-        "line1": "If you want to",
-        "line2": "contact me, feel free to",
-        "line3": "send me a message, thanks!",
-      },
-    },
-    "about_me": {
-      "name": "About Me",
-      "title": "Who Am I?",
-      "intro": {
-        "part1": "Currently, I am a ",
-        "role": "Flutter Developer",
-        "part2": ", and I am also ",
-        "exploring": "exploring mobile native",
-        "part3": " to expand my skills.",
-      },
-      "bullet_point_1":
-          "I have a strong passion for mobile development, always eager to explore new technologies and improve the performance, UI/UX, and overall user experience of the apps I build.",
-      "bullet_point_2":
-          "I believe in lifelong learning and self-improvement. Whether it's mastering a new programming language, optimizing code efficiency, or enhancing my English communication skills, I continuously strive to grow.",
-      "bullet_point_3":
-          "I am always grateful to the mentors and colleagues who have helped me grow, guiding me through both technical challenges and career development.",
-      "bullet_point_4":
-          "I enjoy sharing knowledge and engaging with the developer community, whether through discussions, mentorship, or collaborative projects.",
-      "bullet_point_5":
-          "Oh, this place is where I document my journey, from Flutter development to English learning and beyond. If you're interested, feel free to connect!",
-    },
+    "home": {"title": "Home"},
+    "about_me": {"name": "About Me", "title": "Who Am I?"},
     "my_blogs": "My Blogs",
     "experiences": "Experiences",
     "experience": {
@@ -84,7 +45,6 @@ class CodegenLoader extends AssetLoader {
     },
     "lifetime": "Lifetime",
     "contact": "Contact",
-    "name": "I'm Quyet",
     "social": {
       "facebook": "Follow me on Facebook",
       "telegram": "Contact via Telegram",
@@ -95,47 +55,8 @@ class CodegenLoader extends AssetLoader {
   };
   static const Map<String, dynamic> _vi_VN = {
     "appTitle": "Portfolio",
-    "home": {
-      "title": "Trang chủ",
-      "greeting": "Hi! Tôi là Quyết",
-      "role": "một 'Mobile Engineer'",
-      "sharing": {
-        "line1": "Tôi sẽ chia sẻ",
-        "line2": "kiến thức và những",
-        "line3": "câu chuyện ở đây",
-      },
-      "helping": {
-        "line1": "Tôi hy vọng sẽ giúp",
-        "line2": "mọi người 'một điều gì đó'",
-        "line3": "khi bạn ở đây",
-      },
-      "contact": {
-        "line1": "Nếu bạn muốn",
-        "line2": "liên hệ, hãy thoải mái",
-        "line3": "nhắn tin cho tôi, cảm ơn!",
-      },
-    },
-    "about_me": {
-      "name": "Về tui",
-      "title": "Tôi Là Ai ?",
-      "intro": {
-        "part1": "Hiện tại, tôi là một ",
-        "role": "Flutter Developer",
-        "part2": ", và tôi cũng đang ",
-        "exploring": "tìm hiểu mobile native",
-        "part3": " để mở rộng kỹ năng của mình.",
-      },
-      "bullet_point_1":
-          "Tôi có niềm đam mê mãnh liệt với mobile development, luôn háo hức khám phá công nghệ mới và cải thiện hiệu suất, UI/UX, và trải nghiệm người dùng của các ứng dụng tôi xây dựng.",
-      "bullet_point_2":
-          "Tôi tin vào việc học tập và tự hoàn thiện suốt đời. Dù là làm chủ một ngôn ngữ lập trình mới, tối ưu hóa hiệu quả code, hay nâng cao kỹ năng giao tiếp tiếng Anh, tôi luôn nỗ lực để phát triển.",
-      "bullet_point_3":
-          "Tôi luôn biết ơn những người mentors và đồng nghiệp đã giúp tôi trưởng thành, hướng dẫn tôi vượt qua các thách thức kỹ thuật và phát triển sự nghiệp.",
-      "bullet_point_4":
-          "Tôi thích chia sẻ kiến thức và tham gia cộng đồng developer, dù là thông qua thảo luận, mentoring, hay các dự án hợp tác.",
-      "bullet_point_5":
-          "À, đây là nơi tôi ghi lại hành trình của mình, từ phát triển Flutter đến học tiếng Anh và hơn thế nữa. Nếu bạn quan tâm, hãy kết nối nhé!",
-    },
+    "home": {"title": "Trang chủ"},
+    "about_me": {"name": "Về tui", "title": "Tôi Là Ai ?"},
     "my_blogs": "Blogs tui viết",
     "experiences": "Kinh nghiệm",
     "experience": {
@@ -166,7 +87,6 @@ class CodegenLoader extends AssetLoader {
     },
     "lifetime": "Cuộc sống",
     "contact": "Liên hệ",
-    "name": "Đây là Quyết",
     "social": {
       "facebook": "Theo dõi tôi trên Facebook",
       "telegram": "Liên hệ qua Telegram",
