6adbe7d10dbaf4377845ce47fa36d0a94559f93a
quyetnv <quyetnv.bhsoft@gmail.com>
2025-03-25 08:11:52 +0700

    feat: implement language persistence using shared preferences

--- lib/ui/views/base_screen/view_models/language_view_model.dart
+++ lib/ui/views/base_screen/view_models/language_view_model.dart
@@ -1,15 +1,30 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
-import 'package:riverpod_annotation/riverpod_annotation.dart';
 import 'package:portfolio/core/enums/locale_enum.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 part 'language_view_model.g.dart';
 
 @riverpod
 class LanguageState extends _$LanguageState {
+  static const _languageKey = 'selected_language';
+
   @override
   LocaleEnum build() {
+    _restoreLanguage();
     return LocaleEnum.en;
+  }
+
+  Future<void> _restoreLanguage() async {
+    final prefs = await SharedPreferences.getInstance();
+    final savedLanguage = prefs.getString(_languageKey);
+    if (savedLanguage != null) {
+      state = LocaleEnum.values.firstWhere(
+        (e) => e.name == savedLanguage,
+        orElse: () => LocaleEnum.en,
+      );
+    }
   }
 
   Future<void> toggleLanguage(BuildContext context) async {
@@ -18,6 +33,10 @@ class LanguageState extends _$LanguageState {
         currentLocale == LocaleEnum.en ? LocaleEnum.vi : LocaleEnum.en;
 
     await context.setLocale(newLocale.locale);
+
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setString(_languageKey, newLocale.name);
+
     state = newLocale;
   }
 }
